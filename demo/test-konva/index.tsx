d8c4d44e44b6bcdd539784b5e263ca0b98e8b9ee
fang.kai <fang.kai@cestc.cn>
2024-02-28 17:35:08 +0800

    chore: add preject prettierrc

--- demo/test-konva/index.tsx
+++ demo/test-konva/index.tsx
@@ -1,8 +1,8 @@
-import { createRoot } from "react-dom/client"
-import * as React from 'react'
-import { App } from "./app"
-import "./index.css"
+import { createRoot } from 'react-dom/client';
+import * as React from 'react';
+import { App } from './app';
+import './index.css';
 
-const rootDom = document.getElementById("app")
-const root = createRoot(rootDom)
-root.render(<App />)
+const rootDom = document.getElementById('app');
+const root = createRoot(rootDom);
+root.render(<App />);
